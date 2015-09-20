1f65ba0e7bdacd1f99ffa3e85795bb116af159d1
Dominik <dominik1299@hotmail.com>
2015-09-20 18:59:52 -0400

    Added bullet structure

---
+++ src/Bullet.java
@@ -0,0 +1,19 @@
+
+public class Bullet {
+	Vector2d location;
+	double angle, speed;
+	
+	public Bullet(Vector2d location, double angle, double speed) {
+		this.location = location;
+		this.angle = angle;
+		this.speed = speed;
+	}
+	
+	public void update(double time) {
+		
+	}
+	
+	public void render() {
+		
+	}
+}
